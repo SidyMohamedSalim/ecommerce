d8ebfcdb883932407cb4629e3a9a5becece7eee1
SidyMohamedSalim <sidymamadousalim@gmail.com>
2023-06-11 18:09:07 +0100

    resolve error shop

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -21,17 +21,14 @@ export function SiteHeader() {
           <nav className="flex items-center space-x-1">
             <Link href={siteConfig.links.card} rel="noreferrer">
               <div className="ml-4 flow-root lg:ml-6">
-                <Link
-                  href={siteConfig.links.card}
-                  className="group -m-2 flex items-center p-2"
-                >
+                <div className="group -m-2 flex items-center p-2">
                   <ShoppingBagIcon
                     className="h-6 w-6 shrink-0 "
                     aria-hidden="true"
                   />
                   <span className="ml-2 text-sm font-medium">{total}</span>
                   <span className="sr-only">items in cart, view bag</span>
-                </Link>
+                </div>
               </div>
             </Link>
             <ThemeToggle />
